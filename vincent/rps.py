6eaad3a9984869e3e61b2c41e06429666838013b
Crabo <vincent.qi@hotmail.com>
2020-11-26 22:04:45 -0800

    The funne git commit

---
+++ vincent/rps.py
@@ -0,0 +1,31 @@
+import time
+
+playing = True
+
+while playing:
+    playerInput = input("What will you choose (type 'rock' || 'paper' || 'scissors')")
+    botInput = ""
+
+    if playerInput == "rock":
+        botInput = "My choice: paper"
+    elif playerInput == "paper":
+        botInput = "My choice: scissors"
+    elif playerInput == "scissors":
+        botInput = "My choice: rock"
+    else:
+        print("You didnt type 'rock' 'paper' or 'scissors' :angery:")
+        continue
+
+    print(f"Your choice: {playerInput}\n{botInput}\nyou lost")
+
+    if input("Do you still want to play (type 'T' || 'F')") == "F":
+        playing = False
+    
+    time.sleep(1)
+
+# THIS IS A JOKE AND IS NOT ACTUALLY MADE FOR PLAYING HRNGHRGNHRGNHR GNHRGHRNGHN RGNHRGNHRNGH R NG
+
+
+
+
+
