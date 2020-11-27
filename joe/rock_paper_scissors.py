77ae6e341f51cb7101636b7f36f8011ec14affec
Joe <ravenclawjoe101325@gmail.com>
2020-11-26 21:45:15 -0800

    style: make code even more elegant

--- joe/rock_paper_scissors.py
+++ joe/rock_paper_scissors.py
@@ -1,64 +1,46 @@
-from enum import Enum
 import sys
 import random
-from typing import Union
 
-Choice = Enum("Choice", "rock paper scissors")
-
-# A dictionary of choice -> choice which this choice beats
-# For example, paper beats rock
-COUNTERS = {
-    Choice.paper: Choice.rock,
-    Choice.rock: Choice.scissors,
-    Choice.scissors: Choice.paper,
-}
+# A tuple of tuples (computer_choice, user_choice) where the computer wins
+WIN_CASES = (("rock", "scissor"), ("scissor", "paper"), ("paper", "rock"))
+# Valid choices
+CHOICES = ("rock", "paper", "scissors")
 
 # Terminal escape codes for coloring text
 GREEN_ANSI = "\033[92m"
 END_ANSI = "\033[0m"
 
-MESSAGE_TIE = "🤔 Aw man, it was a tie... Let's play again.\n"
-MESSAGE_SELF_WIN = "🚀 You got absolutely destroyed by the bigbrain computer, as I chose %s versus your %s. Wanna play again?\n"
-MESSAGE_SELF_LOSE = (
-    "🤯 You beat me, human. I chose %s but you chose %s. Now let's play again.\n"
+QUIT_KEYWORDS = (".quit", "q", "stop", "exit")
+
+MESSAGE_WIN = (
+    "🤯 You beat me, human. I chose {0} but you chose {1}. Now let's play again.\n"
 )
-
-
-def get_choice(val: str) -> Union[Choice, None]:
-    if val == "r" or val == "rock":
-        return Choice.rock
-    if val == "p" or val == "paper":
-        return Choice.paper
-    if val == "s" or val == "scissors":
-        return Choice.scissors
-
+MESSAGE_LOSE = "🚀 You got absolutely destroyed by the bigbrain computer, as I chose {0} versus your {1}. Wanna play again?\n"
+MESSAGE_TIE = "🤔 Aw man, it was a tie... Let's play again.\n"
 
 try:
-    user_input = ""
+    user_choice = ""
     while True:
-        user_input = input(
+        user_choice = input(
             f"{GREEN_ANSI}Rock, Paper, Scissors? Type your choice here or .quit to quit: {END_ANSI}"
         ).lower()
 
-        if user_input == ".quit":
+        if user_choice in QUIT_KEYWORDS:
             print("Shutting down the magical rock-paper-scissors AI...")
             sys.exit()
-
-        # Map user choice to enum value
-        choice = get_choice(user_input)
-        if choice is None:
-            print("That wasn't a valid input! Please try again.\n")
+        if user_choice not in CHOICES:
+            print("That wasn't a valid choice, please try again.")
             continue
 
         # Pick a random value
-        bot_choice: Choice = random.choice(list(Choice))
+        bot_choice = random.choice(CHOICES)
 
-        if bot_choice == choice:
-            print("🤔 Aw man, it was a tie... Let's play again.\n")
-        elif COUNTERS[bot_choice] == choice:
-            print(MESSAGE_SELF_WIN.format(bot_choice.name, choice.name))
+        if (bot_choice, user_choice) in WIN_CASES:
+            print(MESSAGE_LOSE.format(bot_choice, user_choice))
+        elif bot_choice == user_choice:
+            print(MESSAGE_TIE)
         else:
-            print(MESSAGE_SELF_LOSE.format(bot_choice.name, choice.name))
+            print(MESSAGE_WIN.format(bot_choice, user_choice))
 
 except KeyboardInterrupt:
     print("\nGood playing with you, human. I'll be shutting down now.")
